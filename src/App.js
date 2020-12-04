b5a1355ef84a1159f9eb703547fef9b8f4627156
Nicholas Anderson <nick.b.anderson4@gmail.com>
2020-12-04 12:55:42 -0600

    container, fixed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
-import {useRoutes} from 'hookrouter';
-
+import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import { Container, Tab, Tabs } from '@material-ui/core'
 import theme from './theme.js'
 
 import HeaderBar from "./HeaderBar.js"
-import TabBar from "./TabBar.js"
 import Player from "./tabs/Player.js";
 import Team from "./tabs/Team.js";
 import Game from "./tabs/Game.js";
 import Season from "./tabs/Season.js";
 
-const routes = {
-    '/': () => <Player />,
-    '/team': () => <Team />,
-    '/game': () => <Game />,
-    '/season': () => <Season />
-}; 
-
 function App() {
-  const routeResult = useRoutes(routes);
+
+  const [tabIndex, setTabIndex] = React.useState(0);
+  const handleTabIndex = (event, newValue) => {
+    setTabIndex(newValue);
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div className="App">
+      <Container
+        padding={4}
+        maxWidth='md'
+      >
           
         <HeaderBar />
-        <TabBar />
-        {routeResult}
-          
-      </div>
+        
+        <Tabs 
+          value={tabIndex} onChange={handleTabIndex} 
+          centered variant='fullWidth'
+          style={{ paddingBottom:10 }}
+          TabIndicatorProps={{ style: { background: "#ffffff" } }}
+        >
+          <Tab label="Item One" value={0} />
+          <Tab label="Item Two" value={1} />
+          <Tab label="Item Three" value={2} />
+          <Tab label="Item Three" value={3} />
+        </Tabs>
+        
+        {tabIndex === 0 && ( <Player /> )}
+        {tabIndex === 1 && (<Team />)}
+        {tabIndex === 2 && (<Game />)}
+        {tabIndex === 3 && (<Season />)}
+
+      </Container>
     </ThemeProvider>
   );
 }
